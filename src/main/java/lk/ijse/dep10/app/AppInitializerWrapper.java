5915a374ac4b88dbf013f2769a521e1967ce4bfa
priman <priman567@gmail.com>
2023-03-25 19:56:41 +0530

    initial commit

---
+++ src/main/java/lk/ijse/dep10/app/AppInitializerWrapper.java
@@ -0,0 +1,4 @@
+package lk.ijse.dep10.app;
+
+public class AppInitializerWrapper {
+}
---
+++ src/main/java/lk/ijse/dep10/app/controller/AddOwnerController.java
@@ -0,0 +1,20 @@
+package lk.ijse.dep10.app.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AddOwnerController {
+
+    @FXML
+    private TextField txtAdminName;
+
+    @FXML
+    private TextField txtAdminPassword;
+
+    @FXML
+    private TextField txtAdminUsername;
+
+    @FXML
+    private TextField txtRePassword;
+
+}
