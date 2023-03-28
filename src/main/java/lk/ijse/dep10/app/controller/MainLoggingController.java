21a894c3e353d03872a311ecd814edc7ecd52373
priman <priman567@gmail.com>
2023-03-28 19:34:51 +0530

    Main Scene UI Change

--- src/main/java/lk/ijse/dep10/app/controller/MainLoggingController.java
+++ src/main/java/lk/ijse/dep10/app/controller/MainLoggingController.java
@@ -1,16 +1,36 @@
 package lk.ijse.dep10.app.controller;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaView;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import lk.ijse.dep10.app.db.DBConnection;
 
-public class MainLoggingController {
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
-    public ToggleGroup Status;
+
+public class MainLoggingController implements Initializable {
+
     public Button btnLogin;
-    public ToggleButton tglShopOwner;
-    public ToggleButton tglCashier;
-    public ToggleGroup tglStatus;
 
 
     @FXML
@@ -18,64 +38,125 @@ public class MainLoggingController {
 
     @FXML
     private TextField txtUsername;
-    Toggle selectedToggle;
-    public void initialize() {
-        txtPassword.textProperty().addListener((observableValue, previous, current) -> {
-            if (txtPassword.getText().isEmpty()) {
-                txtPassword.requestFocus();
-                txtPassword.getStyleClass().add("invalid");
-//                flag=false;
-            }
-        });
+    @FXML
+    private AnchorPane anchorImage;
 
-        tglShopOwner.toggleGroupProperty().addListener((observableValue, previous, current) -> {
-            tglShopOwner.getStyleClass().add("click");
-            System.out.println("aaa");
-        });
+    @FXML
+    private AnchorPane anchorVbox;
 
+    @FXML
+    private Label lblPassword;
 
+    @FXML
+    private Label lblUsername;
+
+    private String name;
+    private int id;
+    private String status;
+
+    @FXML
+    private MediaView mediaView;
+
+    @FXML
+    private AnchorPane root;
+    private MediaPlayer mediaPlayer;
+
+    @FXML
+    private AnchorPane userSideMainAnchor;
+
+    public void initialize(URL url , ResourceBundle resourceBundle) {
+
+//        txtPassword.textProperty().addListener((observableValue, previous, current) -> {
+//            if (txtPassword.getText().isEmpty()) {
+//                txtPassword.requestFocus();
+//                txtPassword.getStyleClass().add("invalid");
+////                flag=false;
+//            }
+//        });
+//        File file = new File("data/video.mp4");
+//        Media media = new Media(file.toURI().toString());
+//        mediaPlayer = new MediaPlayer(media);
+//        System.out.println(mediaPlayer==null);
+//        mediaView.setMediaPlayer(mediaPlayer);
+//        Platform.runLater(()->{
+//           mediaPlayer.play();
+//        });
     }
 
+//    private void videoPlay() {
+//        File file = new File("data/video.mp4");
+//        Media media = new Media(file.toURI().toString());
+//        mediaPlayer = new MediaPlayer(media);
+//        mediaView.setMediaPlayer(mediaPlayer);
+//
+//    }
 
-    public void btnLoginOnAction(ActionEvent actionEvent) {
-        if (isValid()){
-            checkLoginData();
+    public void btnLoginOnAction(ActionEvent actionEvent) throws IOException {
+
+        if (isValid()) {
+            if (checkLoginData()){
+                Stage stage = (Stage) txtPassword.getScene().getWindow();
+                txtPassword.getScene().getWindow().hide();
+                if (status.equals("OWNER")) {
+                    stage.setScene(new Scene(new FXMLLoader(getClass().getResource("/view/OwnerScene.fxml")).load()));
+                    stage.setMaximized(true);
+                    stage.centerOnScreen();
+                    stage.setTitle("OWNER MODE");
+                    stage.show();
+                    System.out.println("admin");
+                }else {
+                    stage.setScene(new Scene(new FXMLLoader(getClass().getResource("/view/CashierScene.fxml")).load()));
+                    stage.setMaximized(true);
+                    stage.centerOnScreen();
+                    stage.setTitle("CASHIER MODE");
+                    stage.show();
+                    System.out.println("cashier");
+                }
+            }
         }
     }
-    private void checkLoginData(){
 
+    private boolean checkLoginData() {
+        Connection connection = DBConnection.getInstance().getConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM Staff WHERE user_name=? AND password=?");
+            preparedStatement.setString(1,txtUsername.getText());
+            preparedStatement.setString(2,txtPassword.getText());
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                name = resultSet.getString("full_name");
+                status = resultSet.getString("status");
+                id = resultSet.getInt("staff_id");
+                System.out.println(name);
+                System.out.println(status);
+                System.out.println(id);
+                return true;
+            }else {
+                new Alert(Alert.AlertType.ERROR, "Invalid Login!").showAndWait();
+                return false;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "Unable to check login data with database").showAndWait();
+            return false;
+        }
     }
-    private boolean isValid(){
-        boolean flag= true;
-        selectedToggle = tglStatus.getSelectedToggle();
+
+    private boolean isValid() {
+        boolean flag = true;
+
         if (txtPassword.getText().isEmpty()) {
             txtPassword.requestFocus();
             txtPassword.getStyleClass().add("invalid");
-            flag=false;
+            flag = false;
         }
         if (txtUsername.getText().isEmpty()) {
             txtUsername.requestFocus();
             txtUsername.getStyleClass().add("invalid");
-            flag=false;
+            flag = false;
         }
-        if (selectedToggle == null) {
-            tglShopOwner.requestFocus();
-            tglCashier.getStyleClass().add("invalid");
-            tglShopOwner.getStyleClass().add("invalid");
-            flag=false;
-        }
+
         return flag;
     }
 
-    public void tglShopOwnerOnAction(ActionEvent actionEvent) {
-        tglCashier.getStyleClass().remove("click");
-        tglShopOwner.getStyleClass().add("click");
-        System.out.println("16541");
-    }
-
-    public void tglCashierOnAction(ActionEvent actionEvent) {
-        tglShopOwner.getStyleClass().remove("click");
-        tglCashier.getStyleClass().add("click");
-        System.out.println("8888");
-    }
 }
